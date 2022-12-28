346faeff4de2b5d5893c0ed64083001306838372
raziq adeel <raziq.adeel7@gmail.com>
2022-12-28 14:33:40 +0100

    registration Logic

--- app.js
+++ app.js
@@ -4,13 +4,15 @@ import web from './routes/web.js';
 
 const app = express();
 const port = process.env.PORT || '3000';
-const DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost:27017';
+const DATABASE_URL = process.env.DATABASE_URL || 'mongodb://127.0.0.1:27017';
 
 //Database Connection
 connectDB(DATABASE_URL);
 
 // Set Template Engine
 app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({ extended: true }));
 
 // Load Routes
 app.use('/', web);
--- routes/web.js
+++ routes/web.js
@@ -4,6 +4,7 @@ import UserController from '../controllers/userController.js';
 
 router.get('/', UserController.home);
 router.get('/registration', UserController.registration);
+router.post('/registration', UserController.createUser);
 router.get('/login', UserController.login);
 
 export default router;
