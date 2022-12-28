c1deab48ee28d008e8e10c408f6bbbb9ce005d68
raziq adeel <raziq.adeel7@gmail.com>
2022-12-28 11:00:36 +0100

    routers

--- routes/web.js
+++ routes/web.js
@@ -3,5 +3,7 @@ const router = express.Router();
 import UserController from '../controllers/userController.js';
 
 router.get('/', UserController.home);
+router.get('/registration', UserController.registration);
+router.get('/login', UserController.login);
 
 export default router;
